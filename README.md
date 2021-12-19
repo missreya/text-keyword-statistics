af3d46fdae2eeb6deeef371178be1a9b69d49df2
Rena <missrenayang@gmail.com>
2021-12-18 21:28:34 -0700

    Added class Filedata, added argparse

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 ## Project Information
-- This script will take all text files along with a keyword file and generate files with text statistics and keyword counts per file
+- This script will take all text files along with a keyword file and generate a file with text statistics and keyword counts
 - Refer to **ds-swe-tech-assessment-prompt.md** for all information
 
-## Outline
-- This file organizes the requirements and includes assumptions made for inputs/outputs to define the limitations of the program
-- Refer to the **Outline.md file** for this information
-
 ## Script Overview
-- main.py will request a file directory which should contain the all of the files of text to process, as well as the keyword file
-- The next input will request the keyword file 
-- The outputs will be generated as text files placed in the same directory with "statistics_" in front of respective text file name
+- User must input a file directory which should contain the all of the files of text to process (defaults to current working directory)
+- User then must provide keyword file name in the same directory (defaults to keyword.txt)
+- The output file will be placed in the same directory titled as "statistics_" with the date and time
+
+### File Requirements:
+- All file inputs are .txt text files
+- File text is in English with words separated by spaces (i.e. not comma separated)
+- Keyword file must be in the same directory as the text files
+- This script uses two optional args: 
+    --textpath: complete file path to folder with text files 
+        Default: current working directory 
+    --keywordfile: file name of keyword text file
+        Default: keyword.txt in current working directory  
--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import os
 import numpy as np
 import re
 import time
+import argparse
+from typing import Dict, List
+from dataclasses import dataclass
+from datetime import datetime
 
-# Change directory to entered Folder Path
-def init_directory(path=None):
-    if path is None:
-        path = input("Enter Folder Path Containing Text Files: ")
-        if len(path) <1: 
-            path = os.getcwd() 
-
-    # Select keyword file. Leaving blank to auto enter default filename "keyword.txt". 
-    keyword_file = input("Enter Keyword File: ")
-    if len(keyword_file) <1: 
-        keyword_file = "keyword.txt" 
-    return path, keyword_file
+@dataclass
+class FileData:
+    lines: List[str]
+    tokens: List[str]
+    keyword_counter: Dict[str, int]
+    duplicate_counter: Dict[str, int]
     
 # Initialization (keywords)
 def init_keyword(keyword_file):
@@ -33,7 +31,7 @@ def file_iterate(path, keyword_file):
     for file in os.listdir(path):
         
         # Skip the keyword and statistics files
-        if file.startswith("statistics_") or file==(keyword_file):
+        if file.startswith("statistics_") or file==(os.path.basename(keyword_file)):
             continue
 
         # Check if file is in text format
@@ -44,15 +42,8 @@ def file_iterate(path, keyword_file):
     return files_to_process
 
 # Text file processing and statistic function
-def process_text_file(file, keyword_list):
-    start_time_file = time.perf_counter()
+def process_text_file(file, keyword_list, statistics: FileData):
     
-    #Initialization (statistics)
-    keyword_counter = dict.fromkeys(keyword_list, 0) #turns keyword list into dict keys with value 0 
-    dupe_counter = dict() #tracks all lines to count dupes
-    line_list = list()
-    token_list = list() 
-
     #Open file for processing in read-only and ignores encoding errors
     with open(file, mode="r", errors="ignore") as f: 
         for line in f:
@@ -61,43 +52,54 @@ def process_text_file(file, keyword_list):
                 continue
 
             #Update counters; dupe counter starts at -1 so the first instance is not counted as a dupe
-            dupe_counter[line] = dupe_counter.get(line, -1) + 1
-            line_list.append(len(line))
-            token_list.append(len(line.split(" ")))
+            statistics.duplicate_counter[line] = statistics.duplicate_counter.get(line, -1) + 1
+            statistics.lines.append(len(line))
+            statistics.tokens.append(len(line.split(" ")))
 
             #Checks for keyword in line and adds count to dictionary
             for keyword in keyword_list:
                 if re.search(r"\b" + str(keyword) + r"\b", line):  
-                    keyword_counter[keyword] = keyword_counter.get(keyword, 0) + 1
+                    statistics.keyword_counter[keyword] = statistics.keyword_counter.get(keyword, 0) + 1
     
+
+def write_output(statistics: FileData):
     # Determine dupes (i.e. keys with value over 1 dupe counter)
-    dup_num = sum(dupe_counter.values())
+    dup_num = sum(statistics.duplicate_counter.values())
     dupes = "num dupes\t" + str(dup_num)
 
     # Calculate line & token results from the stat counter
-    line_stats = "med length\t" + str(np.median(line_list)) +"\n" + "std length\t" + str(np.std(line_list))
-    token_stats = "med tokens\t" + str(np.median(token_list)) +"\n" + "std length\t" + str(np.std(token_list))
-    statistics = line_stats + "\n" + token_stats
+    line_stats = "med length\t" + str(np.median(statistics.lines)) +"\n" + "std length\t" + str(np.std(statistics.lines))
+    token_stats = "med tokens\t" + str(np.median(statistics.tokens)) +"\n" + "std length\t" + str(np.std(statistics.tokens))
+    line_token_stats = line_stats + "\n" + token_stats
 
     #Calculate keyword results from keyword counter
     keywords = str()
-    for key, value in sorted(keyword_counter.items()):
+    for key, value in sorted(statistics.keyword_counter.items()):
         keywords = keywords + key + "\t" + str(value) + "\n"
 
     #Create result text file
-    statistics_file_path = f"{os.path.dirname(file)}\\statistics_{os.path.basename(file)}"
+    now = datetime.now()
+    statistics_file_path = f"{os.getcwd()}\\statistics_{now.strftime('%Y%m%d-%H%M%S')}.txt"
     with open(statistics_file_path, 'w') as r:
-        r.write(f"{dupes}\n{statistics}\n{keywords}")
-    stop_time_file = time.perf_counter()
-    print(f"Completed file: {file} in {stop_time_file - start_time_file:0.4f} seconds") 
+        r.write(f"{dupes}\n{line_token_stats}\n{keywords}")
 
 def main():
-    path, keyword_file = init_directory()
-    keyword_list = init_keyword(keyword_file)
-    files_to_process = file_iterate(path, keyword_file)
+    
+    parser = argparse.ArgumentParser(description='Generates character and keyword statistics from text files')
+    parser.add_argument('-t', '--textpath', type = str, default=f"{os.getcwd()}", help='Folder directory with text files. (Default = working directory)')
+    parser.add_argument('-k', '--keywordfile', type = str, default="keyword.txt", help='Keyword file in same folder as text files. (Default = keyword.txt)')
+    args = parser.parse_args()
+    args.keywordfile = f"{args.textpath}\{args.keywordfile}" 
+
+    keyword_list = init_keyword(args.keywordfile)
+    files_to_process = file_iterate(args.textpath, args.keywordfile)
+
+
+    file_stats = FileData(lines=list(), tokens=list(), keyword_counter=dict.fromkeys(keyword_list, 0), duplicate_counter=dict())
 
     for file in files_to_process:
-        process_text_file(file, keyword_list)
+        process_text_file(file, keyword_list, file_stats)
+    write_output(file_stats)
 
 if __name__ == '__main__':
     main()
